bd71920327495a81329a5d488ff2cdb334d6de79
neong83 <neong83@hotmail.com>
2020-03-28 00:50:57 -0700

    reformat unit test to only validate one subject per test

--- src/tests/test_csv.py
+++ src/tests/test_csv.py
@@ -3,10 +3,17 @@ from tests.base import TestCase
 
 
 class TestCSV(TestCase):
-    def test_read_csv_from_file_name_with_string_csv(self):
-        FILE_NAME = "csv/test_string_input.csv"
-        contents = read_csv_from_file(FILE_NAME)
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.FILE_NAME = "csv/test_string_input.csv"
+
+    def test_read_csv_from_file_name(self):
+        contents = read_csv_from_file(self.FILE_NAME)
         self.assertNotEqual(contents, [])
+
+    def test_read_csv_from_file_name_with_expected_content(self):
+        contents = read_csv_from_file(self.FILE_NAME)
         self.assertTrue("Wiki" in contents)
 
     def test_read_csv_from_file_name_with_file_not_found(self):
