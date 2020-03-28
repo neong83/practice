e02f28345e66bfcc566d74bc27150d67f2693ea8
neong83 <neong83@hotmail.com>
2020-03-27 23:56:25 -0700

    add function to read from a CSV file

--- src/run.py
+++ src/run.py
@@ -1,1 +1,15 @@
-print("this is test output")
+import csv
+from typing import List
+
+
+def read_csv_from_file(file_name: str) -> List[str]:
+    contents = []
+    try:
+        with open(file_name, newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                contents.append(row)
+    except FileNotFoundError:
+        print(f"unable to find {file_name}")
+
+    return contents
