514bc9caf33982a5929d7b141f7141622ade90f3
neong83 <neong83@hotmail.com>
2020-03-28 18:46:22 -0700

    get content formatter before calling open()

--- src/file_io/writer.py
+++ src/file_io/writer.py
@@ -30,7 +30,7 @@ def write_to_file(file_name: str, contents):
     file_full_path = settings.CSV_DIR + file_name
     LOGGER.info(f"Adding new contents {file_full_path}")
 
+    content_formatter = FileOutputType.get_content_formatter(file_name)
     with open(file_full_path, "a") as output_file:
-        content_formatter = FileOutputType.get_content_formatter(file_name)
         output_file.write(content_formatter(contents))
         output_file.close()
