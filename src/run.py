76974ce1f22dc65d5099d44214170f3cc43b1ab4
neong83 <neong83@hotmail.com>
2020-03-28 22:01:50 -0700

    remove code no belongs to run (main app)

--- src/run.py
+++ src/run.py
@@ -1,20 +1,5 @@
-import csv
 import logging
-from typing import List
 
 import settings
 
 LOGGER = logging.getLogger(__name__)
-
-
-def read_csv_from_file(file_name: str) -> List[str]:
-    contents = []
-    try:
-        with open(file_name, newline="") as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                contents += row
-    except FileNotFoundError:
-        LOGGER.warning(f"unable to find file in {file_name}")
-
-    return contents
