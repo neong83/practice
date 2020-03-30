d0cdfcc1cef864b98f0fd106009b6e4dd5be02ba
neong83 <neong83@hotmail.com>
2020-03-29 18:19:57 -0700

    add descending hash list

---
+++ src/sorting/structures.py
@@ -0,0 +1,52 @@
+import logging
+from abc import ABC, abstractmethod
+from string import ascii_letters
+
+from sorting.algorithms.hash_map import (
+    AlphabetIndexCoefficient,
+    hash_first_character_from_word,
+)
+from sorting.algorithms.insertion import get_insert_position_form_sorted_array
+
+LOGGER = logging.getLogger(__name__)
+
+
+class BaseStructure(ABC):  # pragma: no cover
+    @abstractmethod
+    def add(self, value):
+        pass
+
+    def to_list(self):
+        pass
+
+
+class DescendingHashList(BaseStructure):
+    def __init__(self):
+        self.__accident = False
+        self.buckets = [[] for _ in range(26)]
+        self.hash_algorithm = AlphabetIndexCoefficient(acceding=self.__accident)
+
+    def is_ascii_letters(self, value):
+        for letter in value:
+            if letter not in ascii_letters:
+                return False
+        return True
+
+    def add(self, value: str):
+        LOGGER.info(f"Adding `{value}` to bucket")
+        if not value or not self.is_ascii_letters(value):
+            LOGGER.warning(f"Found none ASCII letter(s) in {value}, skip insert")
+            return
+
+        value = value.strip()
+        hash_value = hash_first_character_from_word(self.hash_algorithm, value)
+        position = get_insert_position_form_sorted_array(
+            self.buckets[hash_value], value, self.__accident
+        )
+        self.buckets[hash_value].insert(position, value)
+
+    def to_list(self):
+        result = []
+        for bucket in self.buckets:
+            result += bucket
+        return result
